c9e85605517ab1bd826ea700d5cf2f08500d3380
arturiluck <arturiluck@gmail.com>
2020-05-08 14:45:41 +0300

    WEB-004_add_fix

--- src/Controller/Web/WebController.php
+++ src/Controller/Web/WebController.php
@@ -6,6 +6,8 @@ class WebController extends \Framework\Controller
 {
 	public function index()
 	{
+		//just to show that DB connection has broken
+		$this->container->get('connection');
 		return $this->container->get('response')->renderView('site/index');
 	}
 }
